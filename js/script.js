a8096dc8190a73073b879acb1c7270c2a724e425
anjie_f <amfarmer2022@gmail.com>
2024-07-31 09:09:42 -0500

    Random select

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100.");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
-
 getImage();
+
+//Randomly Select the Index of an Image
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length)
+    // randomIndex();
+    const randomImage = images[randomIndex];
+   // console.log(randomImage);
+};
